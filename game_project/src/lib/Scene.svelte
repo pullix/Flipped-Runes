5c88db51ee26ab366256253b6aa4acb4d3042d3b
Atang Fino <atangfmokamogo@outlook.com>
2024-11-12 16:30:12 +0200

    Threlte Setup

---
+++ game_project/src/lib/Scene.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { T } from "@threlte/core";
+  import { BoxGeometry, DirectionalLight } from "three";
+</script>
+
+<T.Mesh>
+    <T.BoxGeometry />
+    <T.MeshStandardMaterial color="hotpink" />
+</T.Mesh>
+<T.DirectionalLight position={[10, 10, 10]} />
